69e3699b403b2f76bfb1e322384b6b2e1a4d7fe6
Atul Varma <varmaa@gmail.com>
2017-01-02 18:13:33 -0500

    Move stylesheet loading to index.html.

--- src/Main.elm
+++ src/Main.elm
@@ -2,19 +2,16 @@ module Main exposing (main)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (on)
-import Json.Decode exposing (succeed)
 
 import Palette exposing (
   Palette, PaletteMsg, paletteDiv, createPalette, updatePalette
   )
 import Matrix exposing (matrixDiv)
 
-type Message = PaletteMessage PaletteMsg | StylesLoaded
+type Message = PaletteMessage PaletteMsg
 
 type alias Model =
   { palette: Palette
-  , stylesLoaded: Bool
   }
 
 initialPalette : Palette
@@ -30,29 +27,15 @@ initialPalette = createPalette
 model : Model
 model =
   { palette = initialPalette
-  , stylesLoaded = False
   }
-
-styledContent : Model -> List (Html Message) -> Html Message
-styledContent model content =
-  -- This is a temporary hack until we define our own styles.
-  div [] (
-    [ node "link"
-      [ rel "stylesheet"
-      , href "main.css"
-      , on "load" (succeed StylesLoaded)
-      ] []
-    ] ++ (if model.stylesLoaded then content else []))
 
 view : Model -> Html Message
 view model =
-  div [ style [ ("padding", "0 5em") ] ]
-    [ styledContent model
-      [ h2 [] [ text "Palette" ]
-      , Html.map (\m -> PaletteMessage m) (paletteDiv model.palette True)
-      , h2 [] [ text "Accessible Color Combinations" ]
-      , matrixDiv model.palette
-      ]
+  div []
+    [ h2 [] [ text "Palette" ]
+    , Html.map (\m -> PaletteMessage m) (paletteDiv model.palette True)
+    , h2 [] [ text "Accessible Color Combinations" ]
+    , matrixDiv model.palette
     ]
 
 update : Message -> Model -> Model
@@ -60,8 +43,6 @@ update message model =
   case message of
     PaletteMessage msg ->
       {model | palette = updatePalette msg model.palette}
-    StylesLoaded ->
-      {model | stylesLoaded = True}
 
 main =
   Html.beginnerProgram { model = model
